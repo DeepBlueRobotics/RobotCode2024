d816042ab09de83eade4d235b0c03b4db2a11218
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-01-10 19:16:11 -0800

    auto psuedo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,12 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class RobotContainer {
 	//set up subsystems / controllers / limelight
 	
+	public final DigitalInput[] autoSelectors;
+	
+	autoSelectors = new DigitalInput[Math.min(/*amount-of-autos*/, 26)];
+	for(int i = 0; i < autoSelectors.length; i++) autoSelectors[i] = new DigitalInput(i);
+	
+	
   public RobotContainer() {
 		//defaultCommands: elevator, dt
 		//(pass in controller!)
@@ -30,8 +36,31 @@ public class RobotContainer {
 	}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    Command autoPath = null;
+    //PPRobotPath autoPath = new PPRobotPath("Basic 7", drivetrain, false, eventMap);
+    // Command[] autoPath2 = {
+    //   new PPRobotPath("Mid Basic 3", drivetrain, false, eventMap).getPathCommand(true, true), 
+    //   new PPRobotPath("Mid Basic 4", drivetrain, false, eventMap).getPathCommand(false, true)
+    // };
+    // Command[] commands = {
+    //   stopDt(),
+    //   new WaitCommand(0)
+    // };
+    // SequentialCommandGroup autoCommand = new SequentialCommandGroup();
+    // for (int i = 0; i < autoPath2.length; i++) {
+    //   autoCommand.addCommands(autoPath2[i]);
+    //   //autoCommand.addCommands(commands[i]);
+    // }
 
-    
+    for(int i = 0; i < autoSelectors.length; i++) {
+      if(!autoSelectors[i].get()) {
+        System.out.println("Using Path: " + i);
+        autoPath = autoPaths[i];
+        break;
+      }
+    }
+
+    //return autoPath == null ? new PrintCommand("No Autonomous Routine selected") : autoCommand;
+     return autoPath == null ? new PrintCommand("null :(") : autoPath; 
   }
 }
