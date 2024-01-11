0219fce04a6003b96f792521915aef666a4c7983
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-01-10 18:15:00 -0800

    Add subsystem files

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.carlmontrobotics.robotcode2024.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class Intake extends SubsystemBase {
+    public Intake() {
+    }
+
+    @Override
+    public void periodic() {
+     
+		}
+}
