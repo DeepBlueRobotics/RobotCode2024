1244f4990d0f7cc1ca0ec4baed2c6a7b83bf22ff
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-01-14 14:06:00 -0800

    uncomment + copy 2023 constants

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -13,18 +13,146 @@ package org.carlmontrobotics;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+	public static final double g = 9.81; //meters per second squared
+
 	public static final class Drivetrain {
+
+			//#region Subsystem Constants
+
+			public static final double wheelBase = Units.inchesToMeters(19.75);
+			public static final double trackWidth = Units.inchesToMeters(28.75);
+			// "swerveRadius" is the distance from the center of the robot to one of the modules
+			public static final double swerveRadius = Math.sqrt(Math.pow(wheelBase / 2, 2) + Math.pow(trackWidth / 2, 2));
+			// The gearing reduction from the drive motor controller to the wheels
+			// Gearing for the Swerve Modules is 6.75 : 1
+			public static final double driveGearing = 6.75;
+
+			public static final double driveModifier = 1;
+			public static final double wheelDiameterMeters = Units.inchesToMeters(4.0) * 7.36/7.65 /* empirical correction */;
+			public static final double mu = 0.5; /* 70/83.2;  */
+
+			public static final double NEOFreeSpeed = 5676 * (2 * Math.PI) / 60;    // radians/s
+			// Angular speed to translational speed --> v = omega * r / gearing
+			public static final double maxSpeed = NEOFreeSpeed * (wheelDiameterMeters / 2.0) / driveGearing;
+			public static final double maxForward = maxSpeed;
+			public static final double maxStrafe = maxSpeed;
+		 // seconds it takes to go from 0 to 12 volts(aka MAX)
+			public static final double secsPer12Volts = 0.1;
+
+
+
+
+			// maxRCW is the angular velocity of the robot.
+			// Calculated by looking at one of the motors and treating it as a point mass moving around in a circle.
+			// Tangential speed of this point mass is maxSpeed and the radius of the circle is sqrt((wheelBase/2)^2 + (trackWidth/2)^2)
+			// Angular velocity = Tangential speed / radius
+			public static final double maxRCW = maxSpeed / swerveRadius;
+
+			public static final boolean[] reversed = {false, false, false, false};
+			// public static final boolean[] reversed = {true, true, true, true};
+			// Determine correct turnZero constants (FL, FR, BL, BR)
+			public static final double[] turnZero = RobotBase.isSimulation() ?
+					new double[] {0, 0, 0, 0} :
+					new double[] {0, 0 , 0, 0};/*real values here*/
+
+			// kP, kI, and kD constants for turn motor controllers in the order of front-left, front-right, back-left, back-right.
+			// Determine correct turn PID constants
+			public static final double[] turnkP = {0.00374, 0.00374, 0.00374, 0.00374};
+			public static final double[] turnkI = {0, 0, 0, 0};
+			public static final double[] turnkD = {0, 0, 0, 0};
+			public static final double[] turnkS = {0.2, 0.2, 0.2, 0.2};
+			// V = kS + kV * v + kA * a
+			// 12 = 0.2 + 0.00463 * v
+			// v = (12 - 0.2) / 0.00463 = 2548.596 degrees/s
+			public static final double[] turnkV = {0.00463, 0.00463, 0.00463, 0.00463};
+			public static final double[] turnkA = {0.000115, 0.000115, 0.000115, 0.000115};
+
+			// kP is an average of the forward and backward kP values
+			// Forward: 1.72, 1.71, 1.92, 1.94
+			// Backward: 1.92, 1.92, 2.11, 1.89
+			public static final double[] drivekP = {1.82, 1.815, 2.015, 1.915};
+			public static final double[] drivekI = {0, 0, 0, 0};
+			public static final double[] drivekD = {0, 0, 0, 0};
+			public static final boolean[] driveInversion = {false, false, false, false};
+			public static final boolean[] turnInversion = {true, true, true, true};
+
+			public static final double[] kForwardVolts = {0.129, 0.108, 0.14, 0.125};
+			public static final double[] kBackwardVolts = {0.115, 0.169, 0.13, 0.148};
+
+			public static final double[] kForwardVels = {2.910/1.1, 2.970/1.1, 2.890/1.1, 2.930/1.1};
+			public static final double[] kBackwardVels = {2.890/1.1, 2.800/1.1, 2.850/1.1, 2.820/1.1};
+			public static final double[] kForwardAccels = {0.145, 0.149, 0.192, 0.198};
+			public static final double[] kBackwardAccels = {0.192, 0.187, 0.264, 0.176};
+
+			public static final double autoMaxSpeedMps = 0.35 * 4.4;  // Meters / second
+			public static final double autoMaxAccelMps2 = mu * g;  // Meters / seconds^2
+			public static final double autoMaxVolt = 10.0;   // For Drivetrain voltage constraint in RobotPath.java
+			// The maximum acceleration the robot can achieve is equal to the coefficient of static friction times the gravitational acceleration
+			// a = mu * 9.8 m/s^2
+			public static final double autoCentripetalAccel = mu * g * 2;
+
+			public static final boolean isGyroReversed = true;
+
+			// PID values are listed in the order kP, kI, and kD
+			public static final double[] xPIDController = {4, 0.0, 0.0};
+			public static final double[] yPIDController = {4, 0.0, 0.0};
+			public static final double[] thetaPIDController = {0.10, 0.0, 0.001};
+
+			public static final SwerveConfig swerveConfig = new SwerveConfig(wheelDiameterMeters, driveGearing, mu, autoCentripetalAccel, kForwardVolts, kForwardVels, kForwardAccels, kBackwardVolts, kBackwardVels, kBackwardAccels, drivekP, drivekI, drivekD, turnkP, turnkI, turnkD, turnkS, turnkV, turnkA, turnZero, driveInversion, reversed, driveModifier, turnInversion);
+
+			public static final Limelight.Transform limelightTransformForPoseEstimation = Transform.BOTPOSE_WPIBLUE;
+
+			//#endregion
+
+			//#region Ports
+
+			public static final int driveFrontLeftPort = 8;
+			public static final int driveFrontRightPort = 13;
+			public static final int driveBackLeftPort = 5;
+			public static final int driveBackRightPort = 11;
+
+			public static final int turnFrontLeftPort = 7;
+			public static final int turnFrontRightPort = 14;
+			public static final int turnBackLeftPort = 6;
+			public static final int turnBackRightPort = 12;
+
+			public static final int canCoderPortFL = 1;
+			public static final int canCoderPortFR = 2;
+			public static final int canCoderPortBL = 3;
+			public static final int canCoderPortBR = 4;
+
+			//#endregion
+
+			//#region Command Constants
+
+			public static final double kNormalDriveSpeed = 1; // Percent Multiplier
+			public static final double kNormalDriveRotation = 0.5; // Percent Multiplier
+			public static final double kSlowDriveSpeed = 0.4; // Percent Multiplier
+			public static final double kSlowDriveRotation = 0.250; // Percent Multiplier
+			public static final double kAlignMultiplier = 1D/3D;
+			public static final double kAlignForward = 0.6;
+
+			public static final double chargeStationAlignToleranceDeg = 2.5;
+			public static final double chargeStationAlignSpeedMpSPerDeg = 0.3 / 20;
+			public static final double chargeStationAlignTime = 500;
+			public static final double chargeStationAlignFFMpS = 0;
+			public static final double wheelTurnDriveSpeed = 0.0001; // Meters / Second ; A non-zero speed just used to orient the wheels to the correct angle. This should be very small to avoid actually moving the robot.
+
+			public static final double[] positionTolerance = {Units.inchesToMeters(.5), Units.inchesToMeters(.5), 5}; // Meters, Meters, Degrees
+			public static final double[] velocityTolerance = {Units.inchesToMeters(1), Units.inchesToMeters(1), 5}; // Meters, Meters, Degrees/Second
+
+			//#endregion
 	}
-	
+
 	public static final class Arm {
-		
+
 		//all angles in rot here
 		public static final double intakeAngle = -.1;
 		public static final double ampAngle = .3;
 		public static final double speakerAngle = .4;
 		//if needed
 //		public static final trapAngle = 80;
-		
+
 		//Motor Controllers: pid, FF
 		public static final double[] pidVals = new double[] { 0.1, 0.0, 0.1 };
 		// Feedforward
@@ -32,11 +160,11 @@ public final class Constants {
 		public static final double kG = 0.1;
 		public static final double kV = 0.1;
 		public static final double kA = 0.1;
-		
+
 		public static final double MAX_VEL = 1; // rot / s
 		public static final double MAX_ACCEL = 1; // rot / s^2
 		public static TrapezoidProfile.Constraints trapConstraints = new TrapezoidProfile.Constraints(MAX_FF_VEL_AUTO[ARM], MAX_FF_ACCEL[ARM]);
-		
+
 	}
 	public static final class IntakeShooter {
 		//in set() speed
@@ -45,6 +173,6 @@ public final class Constants {
 		public static final double outtakeSpeed = 1;
 
 	}
-	//array or enum with arm and wrist values 
+	//array or enum with arm and wrist values
 
 }
