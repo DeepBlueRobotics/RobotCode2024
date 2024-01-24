e1d0fa3bc0746f315dc61a20f723c60be210714b
stwiggy <144397102+stwiggy@users.noreply.github.com>
2024-01-23 22:02:13 -0800

    prospect code for limelight functions without calculations

--- src/main/java/org/carlmontrobotics/subsystems/Limelight.java
+++ src/main/java/org/carlmontrobotics/subsystems/Limelight.java
@@ -4,12 +4,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   //public boolean targetValid = false;
+  //private double tid, tv, tx, ty, ta;
+  //private double distOffset, horizOffset;
+  //private double horizHeadingError, horizAdjust;
 
   public Limelight() {}
 
   @Override
   public void periodic() {
-    //calls tracking()
+    //tracking();
   }
 
   public void tracking() {
@@ -17,16 +20,37 @@ public class Limelight extends SubsystemBase {
     checks if there is a target and updates targetValid
     puts those values to SmartDashboard
     */
+
+    /* 
+     * tid = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry(“tid”).getDouble(0);
+     * tv = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("tv").getDouble(0);
+     * tx = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("tx").getDouble(0);
+     * ty = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("ty").getDouble(0);
+     * ta = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("ta").getDouble(0);
+     * 
+     * if (tv == 1.0){
+     *   targetValid = true;
+     * } else {
+     *   targetValid = false;
+     * }
+     * 
+     * SmartDashboard.putNumber("apriltag id", tid);
+     * SmartDashboard.putBoolean("target in sight", targetValid);
+     * SmartDashboard.putNumber("horizontal offset", tx);
+     * SmartDashboard.putNumber("vertical offset", ty);
+     * SmartDashboard.putNumber("target area", ta);
+    */
   }
   
   public boolean getTargetValid(){
-    return false; //placeholder
-    //returns targetValid
+    return false;
+    //return targetValid;
   }
 
   public double getTargetID(){
     return 0; //placeholder
     //returns the id of the detected apriltag
+    //return tid;
   }
 
   public double calcDistanceAlignment(){
@@ -44,7 +68,6 @@ public class Limelight extends SubsystemBase {
      * -put hAdjustment to smartdashboard
      * -return hAdjustment 
     */
-
 
   }
 }
