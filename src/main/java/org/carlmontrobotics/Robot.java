528e76c7fad2f0ce9fe550c4d3ea5e887d82b1a8
Team 199 Driver Station Computer <35879629+DriverStationComputer@users.noreply.github.com>
2024-01-27 16:53:01 -0800

    testing distance sensor

--- src/main/java/org/carlmontrobotics/Robot.java
+++ src/main/java/org/carlmontrobotics/Robot.java
@@ -4,33 +4,49 @@
 
 package org.carlmontrobotics;
 
+import org.carlmontrobotics.lib199.MotorConfig;
+import org.carlmontrobotics.lib199.MotorControllerFactory;
+
+import com.playingwithfusion.TimeOfFlight;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
+  private TimeOfFlight distSensor = new TimeOfFlight(10);
+  private double DSdepth = 9.97;
+  private double DSdetectdistance = 14;
+  private CANSparkMax motor = MotorControllerFactory.createSparkMax(9, MotorConfig.NEO);
+
+  public boolean hasGamePiece() {
+    // return false;
+    return getGamePieceDistanceIn() < DSdetectdistance;
+  }
+
+  public double getGamePieceDistanceIn() {
+    return Units.metersToInches((distSensor.getRange() - DSdepth) / 1000 /* Convert mm to m */);
+  }
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    SmartDashboard.putNumber("motor voltage", 0);
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("Detect Distance", getGamePieceDistanceIn());
+    SmartDashboard.putBoolean("Has GUM", hasGamePiece());
+    if (hasGamePiece()) motor.set(0);
+    else motor.set(SmartDashboard.getNumber("motor voltage", 0));
   }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
 
   @Override
   public void autonomousInit() {
@@ -38,14 +54,13 @@ public class Robot extends TimedRobot {
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
+
     }
   }
 
   @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -55,10 +70,16 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void teleopExit() {}
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -66,8 +87,14 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void testExit() {}
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
 }
