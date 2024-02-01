9cd7fea1066176cca0cc448dd01f527b4e3ce925
stwiggy <144397102+stwiggy@users.noreply.github.com>
2024-02-01 13:46:57 -0800

    some functional code

--- src/main/java/org/carlmontrobotics/subsystems/Limelight.java
+++ src/main/java/org/carlmontrobotics/subsystems/Limelight.java
@@ -1,9 +1,12 @@
 package org.carlmontrobotics.subsystems;
 
+import org.carlmontrobotics.Constants;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
-  //public boolean targetValid = false;
   private double tid, tv, tx, ty, ta;
   //private double distOffset, horizOffset;
   //private double horizHeadingError, horizAdjust;
@@ -12,28 +15,21 @@ public class Limelight extends SubsystemBase {
 
   @Override
   public void periodic() {
-    //tracking();
+    tracking();
   }
 
   public void tracking() {
+    tid = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0);
+    tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
     /* gets certain limelight values (ta, tx, ty, tv, tid, etc)
     checks if there is a target and updates targetValid
     puts those values to SmartDashboard
     */
     
-    /* 
-     * tid = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry(“tid”).getDouble(0);
-     * tv = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("tv").getDouble(0);
-     * tx = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("tx").getDouble(0);
-     * ty = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("ty").getDouble(0);
-     * ta = NetworkTableInstance.getDefault().getTable(“limelight”).getEntry("ta").getDouble(0);
-     * 
-     * if (tv == 1.0){
-     *   targetValid = true;
-     * } else {
-     *   targetValid = false;
-     * }
-     * 
+     /* 
      * SmartDashboard.putNumber("apriltag id", tid);
      * SmartDashboard.putBoolean("target in sight", targetValid);
      * SmartDashboard.putNumber("horizontal offset", tx);
@@ -43,8 +39,7 @@ public class Limelight extends SubsystemBase {
   }
   
   public boolean getTargetValid(){
-    return false;
-    //return targetValid;
+    return tv == 1.0;
   }
 
   public double getTargetID(){
@@ -52,21 +47,12 @@ public class Limelight extends SubsystemBase {
     return tid;
   }
 
-  public double calcHorizontalAlignment(){
+  public double calcAngleOffset(){
     return 0; //placeholder
-    /* -calculate hAdjustment
-     * -put hAdjustment to smartdashboard
-     * -return hAdjustment 
-    */
-
+    //-calculates the angle to turn in degrees
   }
 
   public boolean isAligned(double distance){
-    return true; //placeholder
-    /*if MathUtil.applyDeadband(distance, Constants.Limelight.kErrorTolerance) == 0
-     * return true
-     * else
-     * return false
-     */
+    return MathUtil.applyDeadband(distance, Constants.Limelight.errorTolerance) == 0; //if it's within 0.1 of the center
   }
 }
