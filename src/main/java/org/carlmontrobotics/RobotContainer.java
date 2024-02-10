83bc22838c3119367eaa21055b9a8796b2460da9
Team 199 Driver Station Computer <35879629+DriverStationComputer@users.noreply.github.com>
2024-02-09 16:16:18 -0800

    added distance sensor values

--- src/main/java/org/carlmontrobotics/RobotContainer.java
+++ src/main/java/org/carlmontrobotics/RobotContainer.java
@@ -4,12 +4,25 @@
 
 package org.carlmontrobotics;
 
+//import org.carlmontrobotics.subsystems.Intake;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class RobotContainer {
-
-  public RobotContainer() {}
+  private final XboxController controller = new XboxController(0);
+  //Intake intake =  new Intake();
+  public RobotContainer() {
+    /*new JoystickButton(controller, Button.kX.value).whileTrue(intake.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    new JoystickButton(controller, Button.kA.value).whileTrue(intake.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    new JoystickButton(controller, Button.kB.value).whileTrue(intake.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    new JoystickButton(controller, Button.kY.value).whileTrue(intake.sysIdDynamic(SysIdRoutine.Direction.kForward));
+*/
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/org/carlmontrobotics/subsystems/Intake.java
+++ src/main/java/org/carlmontrobotics/subsystems/Intake.java
@@ -2,43 +2,107 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.carlmontrobotics.subsystems;
+/*package org.carlmontrobotics.subsystems;
 
 import org.carlmontrobotics.lib199.MotorConfig;
 import org.carlmontrobotics.lib199.MotorControllerFactory;
 
 import com.playingwithfusion.TimeOfFlight;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+
+import static edu.wpi.first.units.MutableMeasure.mutable;
+import edu.wpi.first.units.Voltage;
+
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class Intake extends SubsystemBase {
+
+ 
+ 
   /** Creates a new Intake. */
-  private TimeOfFlight distSensor = new TimeOfFlight(10);
+
+  /* 
+  private TimeOfFlight distSensor = new TimeOfFlight(12);
   private double DSdepth = 9.97;
   private double DSdetectdistance = 13;
-  private CANSparkMax motor = MotorControllerFactory.createSparkMax(9, MotorConfig.NEO);
-  public Intake() {
-    SmartDashboard.putNumber("motor voltage", 0);
+  private CANSparkMax motor1 = MotorControllerFactory.createSparkMax(7, MotorConfig.NEO);
+  private RelativeEncoder motorEncoder = motor1.getEncoder();
+  private final MutableMeasure<Voltage> voltage = mutable(Volts.of(0));
+  private final MutableMeasure<Velocity<Angle>> angleVelocity = mutable(RotationsPerSecond.of(0));
+  
+ // private final SysIdRoutine SysIdRoutine = 
+   /*  new SysIdRoutine(
+      new SysIdRoutine.Config(),
+      new SysIdRoutine.Mechanism(this::voltageDrive, this::logMotors, this)
+    );
+*/
+//  public Intake() {
+ //   SmartDashboard.putNumber("motor voltage", 0);
+ // }
+
+/*   public void voltageDrive(Measure<Voltage> volts) {
+    motor1.setVoltage(volts.in(Volts));
+  }
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+  return SysIdRoutine.quasistatic(direction);
+} 
+public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+  return SysIdRoutine.dynamic(direction);
+}
+  public void logMotors(SysIdRoutineLog log) {
+      log.motor("intake-motor")
+        .voltage(voltage.mut_replace(motor1.getBusVoltage(),
+        Volts
+      )).angularVelocity(angleVelocity.mut_replace(motorEncoder.getVelocity(),
+      RPM));
 
   }
+  public void loadPerferences() {
+  
+  }
+  */
 
-  @Override
+ /*  @Override
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("Detect Distance", getGamePieceDistanceIn());
     SmartDashboard.putBoolean("Has GUM", hasGamePiece());
-    if (hasGamePiece()) motor.set(0);
-    else motor.set(SmartDashboard.getNumber("motor voltage", 0));
+    
+ //   if (hasGamePiece()) 
+    {
+
+ //     motor1.set(0);    
+    }
+ //   else motor1.set(SmartDashboard.getNumber("motor voltage", 0));
+    
+/*   
   }
+
   public boolean hasGamePiece() {
     // return false;
     return getGamePieceDistanceIn() < DSdetectdistance;
   }
 
   public double getGamePieceDistanceIn() {
-    return Units.metersToInches((distSensor.getRange() - DSdepth) / 1000 /* Convert mm to m */);
-  }
-}
+    return Units.metersToInches((distSensor.getRange() - DSdepth) / 1000 /* Convert mm to m */
+  //}
+
+//}
