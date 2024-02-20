103bc8176f2a7bbbb210dca08a1f43b418a094ce
Brandon <brandonshen123@gmail.com>
2024-02-20 09:27:20 -0800

    Added math for off center notes

--- src/main/java/org/carlmontrobotics/subsystems/IntakeShooter.java
+++ src/main/java/org/carlmontrobotics/subsystems/IntakeShooter.java
@@ -1,6 +1,8 @@
 package org.carlmontrobotics.subsystems;
 
 import static org.carlmontrobotics.Constants.Shooter.*;
+import static org.mockito.ArgumentMatchers.matches;
+
 import org.carlmontrobotics.lib199.MotorConfig;
 import org.carlmontrobotics.lib199.MotorControllerFactory;
 
@@ -21,71 +23,94 @@ public class IntakeShooter extends SubsystemBase {
     private final RelativeEncoder outakeEncoder = outakeMotor.getEncoder();
     private final SparkPIDController pidControllerOutake = outakeMotor.getPIDController();
     private final SparkPIDController pidControllerIntake = intakeMotor.getPIDController();
-    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(kS, kV,kA);
-    private TimeOfFlight distanceSensor = new TimeOfFlight(dsPort1); //make sure id port is correct here
-    private TimeOfFlight distanceSenor2 = new TimeOfFlight(dsPort2); // insert 
+    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(kS, kV, kA);
+    private TimeOfFlight distanceSensor = new TimeOfFlight(dsPort1); // make sure id port is correct here
+    private TimeOfFlight distanceSenor2 = new TimeOfFlight(dsPort2); // insert
     private double dsDepth = 9.97;
     private double detectDistance = 13;
+
     public IntakeShooter() {
         pidControllerOutake.setP(kP);
         pidControllerOutake.setD(kD);
-        outakeEncoder.setPositionConversionFactor(Math.PI/360);
+        outakeEncoder.setPositionConversionFactor(Math.PI / 360);
     }
+
     public double getGamePieceDistance1() {
-        return Units.metersToInches((distanceSensor.getRange() - dsDepth) /1000);
+        return Units.metersToInches((distanceSensor.getRange() - dsDepth) / 1000);
     }
+
     public double getGamePieceDistance2() {
-        return Units.metersToInches((distanceSenor2.getRange() - dsDepth) /1000);
+        return Units.metersToInches((distanceSenor2.getRange() - dsDepth) / 1000);
     }
+
     public boolean gameDistanceSees1st() {
         return getGamePieceDistance1() < detectDistance;
     }
+
     public boolean gameDistanceSees2nd() {
         return getGamePieceDistance2() < detectDistance;
     }
+
     public void senseGamePieceStop() {
-        if (gameDistanceSees1st()){    
-            pidControllerIntake.setReference((-1), CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(-1/60));
-            if(gameDistanceSees2nd()) {
-                pidControllerIntake.setReference(0, CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(0));
+        if (gameDistanceSees1st()) {
+            pidControllerIntake.setReference((-1), CANSparkBase.ControlType.kVelocity, 0,
+                    feedforward.calculate(-1 / 60));
+            if (gameDistanceSees2nd()) {
+                pidControllerIntake.setReference(0, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(0));
             }
+        }
     }
-}
-    //TODO: this shit is complicated as fuck
+    //Find offset of note from the center line using big mathy mathy, god I hope this works chatgpt gave me the formulas :))))))
     public double calculateDistanceSensorNotes() {
-        double a = 4 * Math.pow(distanceBetweenSensors,2);
-        return 1.2;
+        double center = 11.485;
+        double d1 = getGamePieceDistance1();
+        double d2 = getGamePieceDistance2();
+        double r = 7;
+        double xm = (0 + distanceBetweenSensors)/2; //X midpoint between 2 points
+        double ym = (d1+d2)/2; //Y midpoint between 2 points
+        double m = (0-distanceBetweenSensors)/(d2-d1); // Slope
+        double h = xm + r * (1/(Math.sqrt(1+Math.pow(m, 2)))); // x cord of center
+        double k = ym + r * (m/Math.sqrt(1 + Math.pow(m,2)));// y cord of center
+
+        return k - center; //<- offset from the center
+        // double a = 4 * Math.pow(distanceBetweenSensors,2); <- for speaker calcs
+        // return 1.2;
     }
+
     @Override
     public void periodic() {
         senseGamePieceStop();
     }
+
     public void setRPMOutake(double rpm) {
-        pidControllerOutake.setReference(rpm, CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(rpm));
+        pidControllerOutake.setReference(rpm, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(rpm));
     }
+
     public void setRPMintake() {
-        pidControllerIntake.setReference(-6000, CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(-6000));
+        pidControllerIntake.setReference(-6000, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(-6000));
     }
+
     public void shoot(double distance) {
         double rpm = calculateDistanceForRPM();
-        pidControllerOutake.setReference(rpm, CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(rpm));
-        //To be implemented
-    }
-    public void stopOutake() {
-        pidControllerOutake.setReference(0,CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(0));
-    }
-    public void stopIntake() {
-        pidControllerIntake.setReference(0,CANSparkBase.ControlType.kVelocity,0,feedforward.calculate(0));
+        pidControllerOutake.setReference(rpm, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(rpm));
+        // To be implemented
     }
 
+    public void stopOutake() {
+        pidControllerOutake.setReference(0, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(0));
+    }
+
+    public void stopIntake() {
+        pidControllerIntake.setReference(0, CANSparkBase.ControlType.kVelocity, 0, feedforward.calculate(0));
+    }
 
     public double calculateDistanceForRPM() {
-        //returns specific rpm based off of the distance and angle it is in
+        // returns specific rpm based off of the distance and angle it is in
         double angleInDegrees = outakeEncoder.getPosition();
-        double distance = 20; //Add limelight return distance thingy here
+        double distance = 20; // Add limelight return distance thingy here
         double surfaceVelocity = 17;
-        return 17; //super cool mathy stuff after tests and distances
-        //Based off of distance it
+        return 17; // super cool mathy stuff after tests and distances
+        // Based off of distance it
     }
 
 }
