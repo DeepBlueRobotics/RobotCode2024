519029c72360ccafd3e4ee46f25fcf05240bb073
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-03-05 16:56:53 -0800

    fixed feedforward stuff

--- src/main/java/org/carlmontrobotics/subsystems/Arm.java
+++ src/main/java/org/carlmontrobotics/subsystems/Arm.java
@@ -174,7 +174,7 @@ public class Arm extends SubsystemBase {
         double armFeedVolts = armFeed.calculate(getArmPos(), setpoint.velocity);
         if ((getArmPos() < LOWER_ANGLE_LIMIT_RAD)
                 || (getArmPos() > UPPER_ANGLE_LIMIT_RAD)) {
-            armFeedVolts = kG * (COM_ARM_LENGTH_METERS) * Math.cos(getArmPos()) + armFeed.calculate(getCurrentArmGoal().position, 0);
+            armFeedVolts = armFeed.calculate(getArmPos(), 0);
             //kg * cos(arm angle) * arm_COM_length
         }
         armPIDMaster.setReference(setpoint.position, CANSparkBase.ControlType.kPosition, 0, armFeedVolts);
