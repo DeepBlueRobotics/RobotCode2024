dbf70ba8336e10190402d12754ee7a19e999c526
Brandon <brandonshen123@gmail.com>
2024-03-06 14:57:35 -0800

    comments

--- src/main/java/org/carlmontrobotics/commands/ArmTeleop.java
+++ src/main/java/org/carlmontrobotics/commands/ArmTeleop.java
@@ -50,7 +50,7 @@ public class ArmTeleop extends Command {
     double currTime = Timer.getFPGATimestamp();
     double deltaT = currTime - lastTime;
     lastTime = currTime;
-    if (speeds == 0){// If no input, return, seen in issue #24
+    if (speeds == 0){// If no input, return, explained in @brettles comments on issue #24
       return;
     }
     double goalArmRad = goalState.position + speeds * deltaT;
