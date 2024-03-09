34567758becfdec52d9911cc03e6ef943a86fb8d
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-03-09 19:37:15 +0000

    combined eject

--- src/main/java/org/carlmontrobotics/commands/Eject.java
+++ src/main/java/org/carlmontrobotics/commands/Eject.java
@@ -3,28 +3,38 @@ package org.carlmontrobotics.commands;
 import static org.carlmontrobotics.Constants.IntakeShoot.*;
 
 import org.carlmontrobotics.subsystems.IntakeShooter;
+import org.carlmontrobotics.subsystems.Arm;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.Timer;
+import static org.carlmontrobotics.Constants.Arm.*;
 
 public class Eject extends Command {
     //eject until no more game peice
+    // raise arm while setting motors to speed, if motor at speed and arm at goal pos then shoot
     private final IntakeShooter intakeShooter;
+    private final Arm arm = new Arm();
     private final Timer timer = new Timer();
     public Eject(IntakeShooter intakeShooter) {
         this.intakeShooter = intakeShooter;
     }
     @Override
     public void initialize() {
-      intakeShooter.setRPMIntake(EJECT_RPM_INTAKE);
-      intakeShooter.setRPMOutake(EJECT_RPM_OUTAKE);
+      arm.setArmTarget(AMP_ANGLE_RAD);
+      if(arm.armAtSetpoint()){
+        intakeShooter.setRPMIntake(EJECT_RPM_INTAKE);
+        intakeShooter.setRPMOutake(EJECT_RPM_OUTAKE);
+      }
+
       timer.reset();
       timer.start();
     }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-    public void execute() {}
+    public void execute() {
+
+    }
 
   // Called once the command ends or is interrupted.
   @Override
