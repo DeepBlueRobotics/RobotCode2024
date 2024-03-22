cdf832b2184cbc23e595df06c7e4f87fa08f7328
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-03-21 21:21:53 -0700

    threw it all in periodic

--- src/main/java/org/carlmontrobotics/subsystems/Led.java
+++ src/main/java/org/carlmontrobotics/subsystems/Led.java
@@ -6,12 +6,14 @@ import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static org.carlmontrobotics.Constants.Led.*;
+import org.carlmontrobotics.subsystems.IntakeShooter.*;
 
 
 public class Led extends SubsystemBase{
     private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(getLength());
     private final AddressableLED led = new AddressableLED(ledPort);
-    //private double midpoint = Math.floor(ledBuffer.getLength()/2); 
+    private final IntakeShooter intakeshooter = new IntakeShooter();
+    private int midpoint = (int)Math.floor(ledBuffer.getLength()/2); 
     //rounds down
     // public static final double midpoint = Math.floor(ledLength/2); 
     //public static final int ledLength = led.getLength();
@@ -29,6 +31,26 @@ public class Led extends SubsystemBase{
     }
     public int getLength(){
         return ledBuffer.getLength();
+    }
+    public void periodic(){
+        if (intakeshooter.intakeDetectsNote() && !intakeshooter.outakeDetectsNote()) {
+            setLedColor(DETECT_NOTE, 0, midpoint);
+
+        }
+        else if(!intakeshooter.intakeDetectsNote() && intakeshooter.outakeDetectsNote()){
+            setLedColor(DETECT_NOTE,midpoint, getLength());
+        }
+        else if (intakeshooter.intakeDetectsNote() && intakeshooter.outakeDetectsNote()) {
+            setLedColor(HOLDING, 0, getLength());
+
+        }
+        else{
+            setLedColor(DEFAULT_COLOR, 0, getLength());
+        }
+
+
+        
+
     }
     // nothing : setLedColor(defaultColr, 0, ledBuffer.getLength())
     //first tof detect: setLedColor(detectNote, 0, midpoint)
