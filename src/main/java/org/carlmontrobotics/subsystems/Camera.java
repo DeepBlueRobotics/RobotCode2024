e2c9b8ec70cc7c95c913db5cff6b523f14f0ec10
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-03-29 17:48:02 -0700

    Update Camera.java

--- src/main/java/org/carlmontrobotics/subsystems/Camera.java
+++ src/main/java/org/carlmontrobotics/subsystems/Camera.java
@@ -1,4 +1,5 @@
 package org.carlmontrobotics.subsystems;
+import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.MjpegServer;
 import edu.wpi.first.cscore.UsbCamera;
@@ -7,9 +8,21 @@ import edu.wpi.first.util.PixelFormat;
 public class Camera {
     public Camera(){
         UsbCamera usbCamera = new UsbCamera("USB Camera 0", 0);
-        try (MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181)) {
-            mjpegServer1.setSource(usbCamera);
-        }
+        MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1);
+        mjpegServer1.setSource(usbCamera);
+
+        // Creates the CvSink and connects it to the UsbCamera
+        CvSink cvSink = new CvSink("opencv_USB Camera 0");
+        cvSink.setSource(usbCamera);
+
+        // Creates the CvSource and MjpegServer [2] and connects them
+        CvSource outputStream = new CvSource("Blur", PixelFormat.kMJPEG, 640, 480, 30);
+        MjpegServer mjpegServer2 = new MjpegServer("serve_Blur", 1182);
+        mjpegServer2.setSource(outputStream);
+
+
+        //OR
+        
     }
     
 }
