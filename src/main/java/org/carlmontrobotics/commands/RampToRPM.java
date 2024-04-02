2b6afbd5e2623c2e1649b93cebe2a37ebd76856e
aaron345678 <94726964+aaron345678@users.noreply.github.com>
2024-04-02 00:08:52 +0000

    added stopOutake to ramptorpm.java

--- src/main/java/org/carlmontrobotics/commands/RampToRPM.java
+++ src/main/java/org/carlmontrobotics/commands/RampToRPM.java
@@ -34,7 +34,7 @@ public class RampToRPM extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    intake.stopOutake();
     // resets to defaultColor
   }
 
