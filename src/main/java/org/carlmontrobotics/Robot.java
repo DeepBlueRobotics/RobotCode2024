91980f885b2509ca020a8f1d5249b159441b1000
Dean Brettle <dean@brettle.com>
2024-04-23 11:56:08 -0700

    Auto-format modified lines when saving in vscode.
    Also mostly revert immediately prior commit that was made to test this functionality.

--- src/main/java/org/carlmontrobotics/Robot.java
+++ src/main/java/org/carlmontrobotics/Robot.java
@@ -12,88 +12,86 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-
-
 public class Robot extends TimedRobot {
 
-private Command m_autonomousCommand;
-public static Robot robot;
+  private Command m_autonomousCommand;
+  public static Robot robot;
 
-private RobotContainer m_robotContainer;
+  private RobotContainer m_robotContainer;
 
-@Override
+  @Override
 
-public void robotInit() {
-robot = this;
-m_robotContainer = new RobotContainer();
-// SignalLogger.start();
+  public void robotInit() {
+    robot = this;
+    m_robotContainer = new RobotContainer();
+    // SignalLogger.start();
 
-// Record both DS control and joystick data
-DataLogManager.start();
-DriverStation.startDataLog(DataLogManager.getLog());
-FollowPathCommand.warmupCommand().schedule();
-}
+    // Record both DS control and joystick data
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+    FollowPathCommand.warmupCommand().schedule();
+  }
 
-@Override
-public void robotPeriodic() {
-CommandScheduler.getInstance().run();
-}
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
-@Override
-public void disabledInit() {
-// SignalLogger.stop();
-}
+  @Override
+  public void disabledInit() {
+    // SignalLogger.stop();
+  }
 
-@Override
-public void disabledPeriodic() {
-}
+  @Override
+  public void disabledPeriodic() {
+  }
 
-@Override
-public void disabledExit() {
-}
+  @Override
+  public void disabledExit() {
+  }
 
-@Override
-public void autonomousInit() {
-m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-if (m_autonomousCommand != null) {
-m_autonomousCommand.schedule();
-}
-}
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
 
-@Override
-public void autonomousPeriodic() {
-}
+  @Override
+  public void autonomousPeriodic() {
+  }
 
-@Override
-public void autonomousExit() {
-}
+  @Override
+  public void autonomousExit() {
+  }
 
-@Override
-public void teleopInit() {
-if (m_autonomousCommand != null) {
-m_autonomousCommand.cancel();
-}
-}
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
 
-@Override
-public void teleopPeriodic() {
-}
+  @Override
+  public void teleopPeriodic() {
+  }
 
-@Override
-public void teleopExit() {
-}
+  @Override
+  public void teleopExit() {
+  }
 
-@Override
-public void testInit() {
-CommandScheduler.getInstance().cancelAll();
-}
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-@Override
-public void testPeriodic() {
-}
+  @Override
+  public void testPeriodic() {
+  }
 
-@Override
-public void testExit() {
-}
+  @Override
+  public void testExit() {
+  }
 }
