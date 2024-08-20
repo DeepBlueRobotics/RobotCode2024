f31f2098291353b24d6b3d894e38603fb341a712
Team 199 Driver Station Computer <35879629+DriverStationComputer@users.noreply.github.com>
2024-08-20 16:38:09 -0700

    updated offset

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -120,7 +120,7 @@ public final class Constants {
 		public static final boolean ENCODER_INVERTED = true;
 
 		// difference between CoG and arm is .328 rad
-		public static final double ENCODER_OFFSET_RAD = -0.145;
+		public static final double ENCODER_OFFSET_RAD = 6.132;
 		// 0.08 + .328 - 0.404; // - 0.6095;
 
 		// TODO: finish understand why this is broken public static final Measure<Angle>
