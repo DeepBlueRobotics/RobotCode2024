08c13bcb053c1ce9e9708448081fb0d8210696c7
DriverStationComputer <deepbluerobotics@gmail.com>
2024-09-24 17:48:08 -0700

    added ramp to baby mode

--- src/main/java/org/carlmontrobotics/commands/RampMaxRPM.java
+++ src/main/java/org/carlmontrobotics/commands/RampMaxRPM.java
@@ -42,6 +42,6 @@ public class RampMaxRPM extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false || babyMode;
+    return false; // || babyMode;
   }
 }
